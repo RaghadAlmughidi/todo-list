07c54a9c65db480e18e17457dae3f9340bfcf600
rdmug1421 <ralmughidi@gmail.com>
2021-12-24 15:30:21 +0300

    finish GET &POST

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,32 @@ const app=express()
 const db=require('./db1')
 const Todo=require('./todo')
 console.log(Todo);
+app.use(express.json())
 
-app.get('/',(req,res)=>{
+app.get("/",(req,res)=>{
     res.json('GET / WORKING')
 })
-app.get('/tasks',(req,res)=>{
-    res.json('working')
+app.get("/tasks",(req,res)=>{
+   Todo.find({},(err,data)=>{
+     if(err) {
+         console.log('Error: ',err)
+     } else{
+         res.json(data);
+     }
+   }) 
 })
+app.post("/tasks",(req,res)=>{
+    console.log(req.body);
+
+    Todo.create(req.body,(err,newtask)=>{
+       if(err){
+           console.log("ERROR: ",err);
+       }else{
+          res.status(201).json(newtask);
+       }
+    });
+
+});
 
 app.listen(5000,()=>{
     console.log('SERVER WORKING')
